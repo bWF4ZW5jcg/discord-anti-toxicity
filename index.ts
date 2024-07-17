415616b321536abb97d37aabef0a229469dd2a41
o5maks <o4x0n4s@proton.me>
2024-07-17 22:37:36 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import ts from '@tensorflow/tfjs';
+import '@tensorflow/tfjs';
 import '@tensorflow/tfjs-backend-cpu';
 import '@tensorflow/tfjs-backend-webgl';
 
@@ -8,14 +8,14 @@ import Discord from 'discord.js';
 
 const client = new Discord.Client({ intents: 12 });
 
-const discord_token = ''; //Your bot token, https://discord.com/developers/applications
+const discord_token = '';
 const indice = 0.9; //Sensibility
 const messages: Discord.Message[] = []; 
-const interval = 5_000; // Batch interval
+const interval = 5_000;
 
-const model = await toxicityModel.load(indice, []);
+const model = await toxicityModel.load(indice, ['toxicity']);
 
-client.once('ready', async () => {
+client.once('ready', () => {
     console.log('Logged in');
 
     setInterval(async () => {
@@ -24,17 +24,19 @@ client.once('ready', async () => {
                 .splice(0, messages.length)
                 .map(msg => msg.content);
 
-            let results = await model.classify(msgs);
-            for (const index of Array.from<number>({ length: results.length })) {
-                const result = results.at(index);
-
-                if(result && result.results[0]?.match) {
-                    const message = messages.at(index);
-                    if (message) {
-                        message?.delete();
-                    }
+            const results = await model.classify(msgs);
+            results.forEach(({ results }, index) => {
+                if (results.length <= 0) {
+                    return;
                 }
-            }
+
+                const matched = results.filter(({ match }) => match).length > 0;
+                const message = messages.at(index);
+
+                if (matched && message) {
+                    message?.delete();
+                }
+            })
         }
     }, interval);
 });
