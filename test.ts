0346d01ef203cf7438e7445a687dcf28ce62b811
bWF4ZW5jcg <o4x0n4s@proton.me>
2024-09-11 21:28:25 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,30 +1,29 @@
+import '@tensorflow/tfjs';
 import '@tensorflow/tfjs-backend-cpu';
 import '@tensorflow/tfjs-backend-webgl';
-import ts from '@tensorflow/tfjs';
-import toxicityModel from '@tensorflow-models/toxicity';
+import * as Model from '@tensorflow-models/toxicity';
 
 //labels: 'toxicity', 'sexual_explicit', 'threat', 'severe_toxicity', 'obscene', 'insult', 'identity_attack'
 const indice = 0.9;
-const model = await toxicityModel.load(indice, ['toxicity', 'insult']);
+const classifier = await Model.load(indice, ['toxicity', 'insult']);
 
 const messages = [
     "bitch",
-    "bitch-ass",
+    "bitch ass",
     "bitch boy",
     "hello",
-    "Shup up"
+    "Shut up"
 ];
 
-const results = await model.classify(messages);
+const results = await classifier.classify(messages);
 
-results.forEach(({ label, results}) => {
+results.forEach(({ label, results }) => {
     printLn(label);
     results.forEach(({ match }, index) => {
         printLn(`${messages[index]}: ${match}`);
     });
     printLn();
 });
-
 
 function printLn(message?: string) {
     console.log(message || '' + '\n');
